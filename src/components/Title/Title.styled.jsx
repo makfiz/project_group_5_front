e4d1baa916e314ccfb8ea955ed06d7beae54423d
Bohdan <yulia.pakhachuk@gmail.com>
2023-02-18 15:26:47 +0100

    resolved confl

--- src/components/Title/Title.styled.jsx
+++ src/components/Title/Title.styled.jsx
@@ -1,14 +1,31 @@
 import styled from 'styled-components';
 
+const size = {
+  mobile: '320px',
+  tablet: '768px',
+  desktop: '1280px',
+};
+
 export const SectionTitle = styled.h2`
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 42px;
-    margin-bottom: 28px;
-    
-    font-family: 'Manrope';
-    font-weight: 700;
+  margin-top: 42px;
+  margin-bottom: 28px;
+  text-align: center;
+
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33px;
+
+  @media (min-width: ${size.tablet}) {
     font-size: 48px;
     line-height: 66px;
-    color: #111111;
-`
+
+    margin-top: 88px;
+    margin-bottom: 40px;
+  }
+
+  @media (min-width: ${size.desktop}) {
+    margin-top: 60px;
+  }
+
+  color: #111111;
+`;
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from "react";
+import { Title } from "components/Title/Title"; 
+import { NewsList } from "components/News/NewsList/NewsList";
+
 const NewsPage = () => {
-  return <h1>News page</h1>;
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/news")
+      .then(data => data.json())
+      .then(data => setNews(data))
+      .catch(error => console.log(error))
+  }, [news])
+
+  return (
+    <>
+    <Title>News</Title>
+    <NewsList news={news}/>
+    </>
+  );
 };
 
 export default NewsPage;
