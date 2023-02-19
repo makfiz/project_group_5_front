79d208f066f8488ad78f1d627614e4c5dc7e10d6
Grek Maria <grekmariaa@gmail.com>
2023-02-19 09:55:06 +0200

    fix week`s day

--- src/components/OurFriends/WorkDays.jsx
+++ src/components/OurFriends/WorkDays.jsx
@@ -1,15 +1,25 @@
 const WorkDays = ({name, workDays}) => {
-  console.log("name", name)
-  console.log("workDays", workDays)
+  // console.log("name", name)
+// console.log("workDays", workDays)
+  
+  const getTodayIndex = () => {
+    const data = new Date();
+    const day = data.getDay()
 
-  const data = new Date();
-  const day = data.getDay() - 1
-  const workInfoToday = workDays[day]
+    if (day === 0) {
+      return 6
+    }
+    return day - 1
+  }
+  
+  const nomber = getTodayIndex()
+  const workInfoToday = workDays[nomber]
 
 
   return <>
+    {/* <p>Hours work</p> */}
     {workInfoToday.isOpen ?
-      (<p>{workInfoToday.from} - {workInfoToday.to}</p>) :
+      (<p id="workTime">{workInfoToday.from} - {workInfoToday.to}</p>) :
       (<p>--------------------------</p>)}
   </>
 }
