260490c3fb3afeff382a99dda85ecec38da3d440
makfiz <99556605+makfiz@users.noreply.github.com>
2023-02-20 13:59:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 // import { useDispatch, useSelector } from 'react-redux';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
