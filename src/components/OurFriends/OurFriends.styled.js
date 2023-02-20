3b0b91da500895e25e8e4757b3e38d5a7973bf77
Grek Maria <grekmariaa@gmail.com>
2023-02-20 11:58:51 +0200

    styled modal work hours

--- src/components/OurFriends/OurFriends.styled.js
+++ src/components/OurFriends/OurFriends.styled.js
@@ -1,26 +1,77 @@
 import styled from 'styled-components';
 
+const size = {
+  mobile: '320px',
+  tablet: '768px',
+  desktop: '1280px',
+};
+
 export const BoxFriendsList = styled.div`
   /* border: 1px red solid; */
-  padding: 16px;
+  display: flex;
+  justify-content: space-around;
+  /* margin: 0 auto; */
+  @media screen and (min-width: 320px) {
+    padding: 20px;
+  }
+
+  @media screen and (min-width: 768px) {
+    padding: 32px;
+
+    display: block;
+  }
+
+  @media screen and (min-width: 1280px) {
+    padding: 16px;
+  }
 `;
 
 export const FriendsList = styled.ul`
   display: grid;
-  grid-gap: 32px;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+
+  @media screen and (min-width: 320px) {
+    grid-gap: 12px;
+  }
+
+  /* width: 280px; */
+  /* grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr); */
+
+  @media screen and (min-width: 768px) {
+    /* width: 768px; */
+    /* width: 704px; */
+    grid-gap: 32px;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  @media screen and (min-width: 1280px) {
+    /* width: 1248px; */
+    grid-gap: 32px;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
 `;
 
 export const FriendsItem = styled.li`
   background-color: #fff;
   border-radius: 40px;
   box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
-  padding: 16px 4px;
 
-  width: auto;
+  width: 280px;
+  padding: 12px;
   /* width: 395px; */
   /* height: 287px; */
+
+  @media (min-width: ${size.tablet}) {
+    width: auto;
+    /* width: calc(90vw / 2); */
+  }
+
+  @media (min-width: ${size.desktop}) {
+    /* width: 395px; */
+    padding: 16px 4px;
+  }
 `;
 
 export const FriendTitle = styled.h2`
@@ -28,13 +79,25 @@ export const FriendTitle = styled.h2`
   margin: 0;
   padding: 0;
   padding-bottom: 16px;
+
   a {
     color: ${props => props.theme.colors.hoverGoHome};
-    font-size: 20px;
     font-weight: ${props => props.theme.fontWeight.bold};
+    font-size: 12px;
+    line-height: 16px;
+
     :hover {
-      /* hover */
       color: ${props => props.theme.colors.navAccent};
+    }
+
+    @media (min-width: ${size.tablet}) {
+      font-size: ${props => props.theme.fontSizes.m};
+      line-height: 22px;
+    }
+
+    @media (min-width: ${size.tablet}) {
+      font-size: 20px;
+      line-height: 27px;
     }
   }
 `;
@@ -42,6 +105,7 @@ export const FriendTitle = styled.h2`
 export const BoxFriendItem = styled.div`
   /* border: 1px blue solid; */
   display: flex;
+  position: relative;
 `;
 
 export const BoxFriendLogo = styled.div`
@@ -59,13 +123,28 @@ export const BoxFriendInfo = styled.div`
 //FriendInfoList
 
 export const FriendInfoItem = styled.li`
-  margin-bottom: 12px;
+  margin-bottom: 4px;
 
   /* font-family: 'Manrope'; */
-  font-style: normal;
+  /* font-style: normal; */
   font-weight: ${props => props.theme.fontWeight.medium};
-  font-size: ${props => props.theme.fontSizes.m};
-  line-height: 22px;
+  font-size: 12px;
+  line-height: 16px;
+
+  @media (min-width: ${size.tablet}) {
+    margin-bottom: 8px;
+
+    font-size: ${props => props.theme.fontSizes.s};
+    line-height: 19px;
+  }
+
+  @media (min-width: ${size.desktop}) {
+    margin-bottom: 12px;
+
+    font-size: ${props => props.theme.fontSizes.m};
+    line-height: 22px;
+  }
+
   a {
     /* скидання стилів */
     text-decoration: none;
@@ -78,4 +157,54 @@ export const FriendInfoItem = styled.li`
       color: ${props => props.theme.colors.hoverGoHome};
     }
   }
+`;
+
+export const BoxModal = styled.div`
+  display: block;
+  /* position: fixed; */
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.1);
+`;
+
+export const ModalContent = styled.div`
+  background-color: ${props => props.theme.colors.white};
+  margin: 10% auto;
+  padding: 12px;
+  width: 120px;
+  position: absolute;
+  top: 5%;
+  left: 50%;
+  border: 1px solid ${props => props.theme.colors.hoverGoHome};
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+
+  margin-bottom: 4px;
+
+  font-weight: ${props => props.theme.fontWeight.medium};
+  font-size: 12px;
+  line-height: 16px;
+
+  @media (min-width: ${size.tablet}) {
+    left: 40%;
+    margin: 5% auto;
+  }
+
+  @media (min-width: ${size.desktop}) {
+    left: 58%;
+    top: 7%;
+    margin: 5% auto;
+  }
+`;
+
+export const ModalText = styled.li`
+  margin-bottom: 4px;
+
+  font-weight: ${props => props.theme.fontWeight.medium};
+  font-size: 12px;
+  line-height: 16px;
 `;
--- src/components/OurFriends/OurFriendsItem.jsx
+++ src/components/OurFriends/OurFriendsItem.jsx
@@ -1,8 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+import Modal from "./Modal";
 import { FriendTitle, BoxFriendItem, BoxFriendLogo, BoxFriendInfo, FriendInfoItem } from "./OurFriends.styled";
 import WorkDays from "./WorkDays";
 
-const OurFriendsItem = ({friend}) => {
-  const { title, imageUrl, address, email, phone, workDays, addressUrl, url } = friend;
+const OurFriendsItem = ({ friend }) => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+  
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      console.log("event", event)
+      if (event.keyCode === 27) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+    // console.log('toggle modal')
+  };
+
+  const closeModal = (isOpen) => {
+    setIsOpen(false);
+  };
+
+  const handleClose = (event) => {
+    closeModal();
+  };
+
+  const {
+    title,
+    imageUrl,
+    address,
+    email,
+    phone,
+    workDays,
+    addressUrl,
+    url } = friend;
   // console.log("workDays", workDays)
 
   const mailto = 'mailto:' 
@@ -12,19 +54,23 @@ const OurFriendsItem = ({friend}) => {
 
   return <>
           <FriendTitle> <a href={url} target="_blank" rel="noreferrer">{title}</a></FriendTitle>
-          <BoxFriendItem>
+          <BoxFriendItem >
             <BoxFriendLogo>
               <img src={imageUrl} alt="Logo" className="FriendLogo" width="158" height="112"></img>
             </BoxFriendLogo>
             <BoxFriendInfo>
               <ul className="FriendInfoList">
-                <FriendInfoItem>Time: {workDays ? (<WorkDays workDays={workDays} name={title} />) : (<p>--------------------------</p>) }</FriendInfoItem>
+          <FriendInfoItem onClick={toggleModal}>Time: {workDays ?
+              (<WorkDays workDays={workDays} name={title} />) :
+              (<p>--------------------------</p>)}
+          </FriendInfoItem>
                 <FriendInfoItem>Email: {email ? (<a href={hrefMail}>{email}</a>) : (<p>--------------------------</p>) }</FriendInfoItem>
                 <FriendInfoItem>Phone: {phone ? (<a href={hreftel}>{phone}</a>) : (<p>--------------------------</p>) }</FriendInfoItem>
                 <FriendInfoItem>Address: {address ? (<address><a href={addressUrl} target="_blank" rel="noreferrer">{address}</a></address>) : (<p>--------------------------</p>) }</FriendInfoItem>
               </ul>
-            </BoxFriendInfo>
-          </BoxFriendItem>
+      </BoxFriendInfo>
+        {isOpen && (<Modal workDays={workDays} onClick={handleClose} />)}
+      </BoxFriendItem>
   </>
 }
 
