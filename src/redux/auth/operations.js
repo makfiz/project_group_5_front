1e7ba46d00a758c9e9d4648493634b165e03fc61
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-02-21 18:39:27 +0200

    pagination

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,7 +3,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://petssuport4815162342api.onrender.com/api';
 
-
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
@@ -23,30 +22,28 @@ export const registration = createAsyncThunk(
 );
 
 const unsetToken = () => {
-    return axios.defaults.headers.common.Authorization = '';
+  return (axios.defaults.headers.common.Authorization = '');
 };
 
 const setToken = token => {
-    return axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  return (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
 };
 
 export const login = createAsyncThunk('auth/login', async credentials => {
-    try {
-        const { data } = await axios.post('/users/login', credentials);
-        token.setToken(data.token);
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const { data } = await axios.post('/users/login', credentials);
+    token.setToken(data.token);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export const logout = createAsyncThunk('auth/logout', async () => {
-    try {
-        await axios.get('/users/logout');
-        token.unsetToken();
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await axios.get('/users/logout');
+    token.unsetToken();
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-
