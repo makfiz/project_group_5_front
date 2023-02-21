6695f92355301f25bc1b30be74390df3841e5b0d
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-02-21 02:45:16 +0200

    feature/auth-redux

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,17 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { registration } from './operations';
+import { login, logout } from './operations.js';
 
 const initialState = {
-  user: { name: null, email: null },
+  user: { id: null, email: null },
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  error: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {},
+  extraReducers: {
+    [login.pending](state) { state.isRefreshing = true; },
+    [login.fulfilled](state, action) { state.isRefreshing = false; state.error = null; state.user.id = action.payload.id; state.user.email = action.payload.email; state.token = action.payload.token; state.isLoggedIn = true; },
+    [login.rejected](state, action) { state.isRefreshing = false; state.error = action.payload; },
+    // [logout.pending](state) { state.isRefreshing = true; },
+    [logout.fulfilled](state, action) { state.isRefreshing = false; state.error = null; state.user.id = null; state.user.email = null; state.token = null; state.isLoggedIn = false; },
+    [logout.rejected](state, action) { state.isRefreshing = false; state.error = action.payload; },
+  },
 });
 
 export const authReducer = authSlice.reducer;
