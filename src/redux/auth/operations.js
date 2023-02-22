133854883bbb2f96c8839a3f69248aed4ad4e0a8
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2023-02-23 00:00:23 +0200

    authLogin-fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -22,7 +22,7 @@ export const registration = createAsyncThunk(
   '/auth/registration',
   async (credentials, thunkAPI) => {
     try {
-      const { data } = await axios.post('users/signup', credentials);
+      const { data } = await axios.post('/users/signup', credentials);
       Notiflix.Notify.success('Success! Now you can login ✔', {
         timeout: 2500,
       });
@@ -45,13 +45,27 @@ export const registration = createAsyncThunk(
   }
 );
 
-const login = createAsyncThunk('/auth/login', async credentials => {
+const login = createAsyncThunk('/auth/login', async (credentials, thunkAPI) => {
     try {
       const { data } = await axios.post('/users/login', credentials);
       setToken(data.token);
+      Notiflix.Notify.success('Success! Now you are logedin ✔', {
+        timeout: 2500,
+      });
       return data;
     } catch (error) {
-        console.log(error);
+      console.log(error.message);
+      if (error.message === 'Request failed with status code 401') {
+        Notiflix.Notify.failure('Email, or password is wrong, or email is not verified. Please check your e-mail ⚠', {
+          timeout: 2500,
+        });
+      };
+      if (error.message === 'Request failed with status code 400') {
+        Notiflix.Notify.failure('Not valid format of email or password ⚠', {
+          timeout: 2500,
+        });
+      };
+      return thunkAPI.rejectWithValue(error.message);
     }
 });
 
