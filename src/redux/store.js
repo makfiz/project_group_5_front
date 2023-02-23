8ab3ba7a5eb95abd6e6bbfb7756b9a3a7027945f
Maksym Hlanskov <makfiz@gmail.com>
2023-02-23 19:18:45 +0200

    refreshUserLoader

--- src/redux/store.js
+++ src/redux/store.js
@@ -48,7 +48,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-      }),
+    }),
     // petsApi.middleware,
   ],
 });
