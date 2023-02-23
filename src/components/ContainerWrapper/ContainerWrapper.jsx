3d0911a6d90cea4a8b02b4b90e2aeab510025e40
Dmytro Voronievskyi <vdrabota@icloud.com>
2023-02-23 23:43:00 +0100

    rename Container

--- src/components/ContainerWrapper/ContainerWrapper.jsx
+++ src/components/ContainerWrapper/ContainerWrapper.jsx
@@ -1,4 +1,4 @@
-import { StyledContainer } from './Container.styled';
+import { StyledContainer } from './ContainerWrapper.styled';
 
 export const Container = ({ children }) => {
   return <StyledContainer>{children}</StyledContainer>;
