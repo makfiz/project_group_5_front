6e9221ae992682de6aa3dd7f2bb8efac1b8a0a5a
Grek Maria <grekmariaa@gmail.com>
2023-02-23 21:28:06 +0200

    today's working time
    selection of today's working time

--- src/components/OurFriends/Modal/WorkHours.jsx
+++ src/components/OurFriends/Modal/WorkHours.jsx
@@ -2,14 +2,22 @@ import { ModalText } from './Modal.styled';
 
 const WorkHours = ({ workDays }) => {
   const dayLabels = ['MN', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
+  const data = new Date();
+  const getTodayIndex = (data) => (data.getDay() + 6) % 7;
+  const today = getTodayIndex(data)
 
   return (
     <ul>
       {workDays.map((day, index) => (
-        <ModalText key={day._id}>{dayLabels[index]} {day.from} - {day.to}</ModalText>
+        <ModalText
+          key={day._id}
+          className={today === index ? 'highlight' : ''} // додати клас "highlight", якщо це поточний день тижня
+        >
+          {dayLabels[index]} {day.from} - {day.to}
+        </ModalText>
       ))}
     </ul>
   );
-}
+};
 
 export default WorkHours
