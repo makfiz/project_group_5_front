d19b7cede48a9ab5478b7b5800a1a7f884c77a70
Maksym Hlanskov <makfiz@gmail.com>
2023-02-24 11:03:57 +0200

    Update petsOperations.js

--- src/redux/Pets/petsOperations.js
+++ src/redux/Pets/petsOperations.js
@@ -7,14 +7,14 @@ export const addPet = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const { data } = await axios.post('/pets', credentials);
-      // Notiflix.Notify.success('Success! Now you can login ✔', {
+      // Notiflix.Notify.success('Success!', {
       //   timeout: 3000,
       // });
       return data;
     } catch (error) {
       console.log(error.message);
       // if (error.message === 'Request failed with status code 409') {
-      //   Notiflix.Notify.failure('This user is already registered ⚠', {
+      //   Notiflix.Notify.failure('⚠', {
       //     timeout: 3000,
       //   });
       // }
@@ -43,10 +43,10 @@ export const updatePetImage = createAsyncThunk(
 
 export const deletePet = createAsyncThunk(
   '/deletePet',
-  async (credentials, thunkAPI) => {
+  async (petId, thunkAPI) => {
     try {
       console.log('credentials', credentials);
-      const { data } = await axios.delete(`/${payload._id}`, credentials);
+      const { data } = await axios.delete(`pets/${petId}`, credentials);
 
       console.log(data);
       return data;
@@ -56,104 +56,6 @@ export const deletePet = createAsyncThunk(
     }
   }
 );
-
-// const login = createAsyncThunk('/auth/login', async (credentials, thunkAPI) => {
-//   try {
-//     const { data } = await axios.post('/users/login', credentials);
-//     setToken(data.token);
-//     Notiflix.Notify.success('Success! Now you are logedin ✔', {
-//       timeout: 3000,
-//     });
-//     return data;
-//   } catch (error) {
-//     console.log(error.message);
-//     if (error.message === 'Request failed with status code 401') {
-//       Notiflix.Notify.failure(
-//         'Email, or password is wrong, or email is not verified. Please check your e-mail ⚠',
-//         {
-//           timeout: 3000,
-//         }
-//       );
-//     }
-//     if (error.message === 'Request failed with status code 400') {
-//       Notiflix.Notify.failure('Not valid format of email or password ⚠', {
-//         timeout: 3000,
-//       });
-//     }
-//     return thunkAPI.rejectWithValue(error.message);
-//   }
-// });
-
-// const logout = createAsyncThunk('/auth/logout', async () => {
-//   try {
-//     await axios.get('/users/logout');
-//     unsetToken();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// const googleApi = createAsyncThunk('auth/google', async credentials => {
-//   try {
-//     setToken(credentials.token);
-//     return credentials;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// export const refresh = createAsyncThunk('auth/refresh', async (_, thunkAPI) => {
-//   const state = thunkAPI.getState();
-//   const persistedToken = state.auth.token;
-//   if (persistedToken === null || '') {
-//     return thunkAPI.rejectWithValue('unauthorized access');
-//   }
-//   try {
-//     setToken(persistedToken);
-//     const response = await axios.get('/users/me');
-//     return response.data;
-//   } catch (error) {
-//     return thunkAPI.rejectWithValue(error.message);
-//   }
-// });
-
-// export const userUpdate = createAsyncThunk(
-//   '/auth/userUpdate',
-//   async (credentials, thunkAPI) => {
-//     try {
-//       const { data } = await axios.patch('/users/update', credentials);
-//       Notiflix.Notify.success('Success! User updated! ✔', {
-//         timeout: 3000,
-//       });
-
-//       return data;
-//     } catch (error) {
-//       console.log(error.message);
-//       if (error.message === 'Request failed with status code 404') {
-//         Notiflix.Notify.failure('User not found. Please register user! ⚠', {
-//           timeout: 3000,
-//         });
-//       }
-//       if (error.message === 'Request failed with status code 401') {
-//         Notiflix.Notify.failure(
-//           'jwt token is expired or not valid. Please login user! ⚠',
-//           {
-//             timeout: 3000,
-//           }
-//         );
-//       }
-//       if (error.message === 'Request failed with status code 400') {
-//         Notiflix.Notify.failure(
-//           'Not valid format of request body (check types of yuor request data) ⚠',
-//           {
-//             timeout: 3000,
-//           }
-//         );
-//       }
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
 
 const petsOperations = {
   addPet,
