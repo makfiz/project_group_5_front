33d1bdbedf2b5e5a975c0465e6f69e901dff9d28
Bohdan <yulia.pakhachuk@gmail.com>
2023-02-25 19:59:08 +0100

    fix container

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,14 +1,31 @@
 import styled from 'styled-components';
 
+
 export const ContainerStyled = styled.div`
-  width: 320px;
-  margin-right: auto;
+  width: 100%;
+  padding-left: 16px;
+  padding-right: 16px;
   margin-left: auto;
+  margin-right: auto;
+
+  @media (min-width: ${p => p.theme.screenSizes.mobile}) {
+    max-width: ${p => p.theme.screenSizes.mobile};
+
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 
   @media (min-width: ${p => p.theme.screenSizes.tablet}) {
-    width: 768px;
+    max-width: ${p => p.theme.screenSizes.tablet};
+
+    padding-left: 32px;
+    padding-right: 32px;
   }
+
   @media (min-width: ${p => p.theme.screenSizes.desktop}) {
-    width: 1280px;
+    max-width: ${p => p.theme.screenSizes.desktop};
+
+    padding-left: 16px;
+    padding-right: 16px;
   }
 `;
