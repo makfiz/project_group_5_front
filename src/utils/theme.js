b1bfb1928e55ed6998dea53419d6971239a969a0
Grek Maria <grekmariaa@gmail.com>
2023-02-26 10:01:49 +0200

    add color for dark theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -15,6 +15,10 @@ export const theme = {
     bold: 700,
   },
   colors: {
+    // light
+    bgc: '#fdf7f2',
+    bgcContent: '#ffffff',
+    text: '#000000',
     white: '#ffffff',
     black: '#000000',
     grey: '#a1abb6',
@@ -34,9 +38,8 @@ export const theme = {
     petModalplaceholder: 'rgba(27, 27, 27, 0.6)',
     userAddPetBtnBg: '#F59256',
     userAddPetBtnBgHover: '#ff6101',
-    closeIconColor: '#FDF7F2',
-    favoriteBg: 'rgba(255, 255, 255, 0.6)',
-    articleText: "#111321",
+    newsGradient1: '#ff634e',
+    newsGradient2: '#ffdf48',
   },
   shadows: {
     shadow: '0px 0px 5px 0px rgba(0, 0, 0, 0.75)',
@@ -66,7 +69,152 @@ export const theme = {
   transition: {
     onHover: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
   },
-  gradient: {
-    newsDecoration: "90deg, #ff634e 0%, #ffdf48 105.44%",
+};
+
+export const themeDark = {
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    ms: '20px',
+    l: '24px',
+    ls: '36px',
+    lm: '28px',
+    xl: '48px',
+  },
+  fontWeight: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  colors: {
+    // dark
+    bgc: '#111212',
+    bgcContent: '#1C1F20',
+    text: '#ffffff',
+    white: '#000000',
+    black: '#ffffff',
+    grey: '#a1abb6',
+    navAccent: '#2196f3',
+    hoverGoHome: '#56B9F5',
+    titleColor: '#E2E2E2',
+    //^
+    placeholderColor: '#E2E2E2',
+    // ??
+    regModBack: '#000000',
+    //
+    transparent: 'transparent',
+    regModalTitleColor: ' #E2E2E2',
+    regModalLinkText: 'rgba(226, 226, 226, 0.6)',
+    //
+    regModalLink: '#3091EB',
+    regModalActiveBtn: '#56B9F5',
+    //
+    regModalplaceholderColor: 'rgba(226, 226, 226, 0.6)',
+    error: 'red',
+    deleteBtnColor: '#019FFF',
+    //
+    petModalplaceholder: 'rgba(27, 27, 27, 0.6)',
+    userAddPetBtnBg: '#56B9F5',
+    userAddPetBtnBgHover: '#019FFF',
+  },
+  shadows: {
+    shadow: '0px 0px 5px 0px rgba(0, 0, 0, 0.75)',
+    insideShadow: 'inset 5px -4px 86px 0px rgba(0,0,0,0.22)',
+    boxShadow: '2px 2px 2px 1px rgb(0 0 0 / 20%)',
+    inputShadow: '7px 4px 14px rgba(49, 21, 4, 0.07)',
+    regModalShadow: '7px 4px 14px rgba(0, 0, 0, 0.11)',
+    hoursWorkModalShadow: '4px 4px 8px rgba(0, 0, 0, 0.25)',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  radii: {
+    normal: '5px',
+    standard: '10px',
+    medium: '20px',
+    large: '40px',
+    round: '50%',
+  },
+  borders: {
+    profile: '1px solid rgb(86 86 86/48%)',
+    statCard: '1px solid #ffffff',
+  },
+  screenSizes: {
+    mobile: '320px',
+    tablet: '768px',
+    desktop: '1280px',
+  },
+  transition: {
+    onHover: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
+
+export const themeLight = {
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    ms: '20px',
+    l: '24px',
+    ls: '36px',
+    lm: '28px',
+    xl: '48px',
+  },
+  fontWeight: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  colors: {
+    // light
+    bgc: '#fdf7f2',
+    bgcContent: '#ffffff',
+    text: '#000000',
+    white: '#ffffff',
+    black: '#000000',
+    grey: '#a1abb6',
+    navAccent: '#2196f3',
+    hoverGoHome: '#F59256',
+    titleColor: '#111111',
+    placeholderColor: '#535353',
+    regModBack: '#ffffff',
+    transparent: 'transparent',
+    regModalTitleColor: ' #111111',
+    regModalLinkText: 'rgba(17, 17, 17, 0.6)',
+    regModalLink: '#3091EB',
+    regModalActiveBtn: '#F59256',
+    regModalplaceholderColor: 'rgba(17, 17, 17, 0.6)',
+    error: 'red',
+    deleteBtnColor: '#FF6101',
+    petModalplaceholder: 'rgba(27, 27, 27, 0.6)',
+    userAddPetBtnBg: '#F59256',
+    userAddPetBtnBgHover: '#ff6101',
+  },
+  shadows: {
+    shadow: '0px 0px 5px 0px rgba(0, 0, 0, 0.75)',
+    insideShadow: 'inset 5px -4px 86px 0px rgba(0,0,0,0.22)',
+    boxShadow: '2px 2px 2px 1px rgb(0 0 0 / 20%)',
+    inputShadow: '7px 4px 14px rgba(49, 21, 4, 0.07)',
+    regModalShadow: '7px 4px 14px rgba(0, 0, 0, 0.11)',
+    hoursWorkModalShadow: '4px 4px 8px rgba(0, 0, 0, 0.25)',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  radii: {
+    normal: '5px',
+    standard: '10px',
+    medium: '20px',
+    large: '40px',
+    round: '50%',
+  },
+  borders: {
+    profile: '1px solid rgb(86 86 86/48%)',
+    statCard: '1px solid #ffffff',
+  },
+  screenSizes: {
+    mobile: '320px',
+    tablet: '768px',
+    desktop: '1280px',
+  },
+  transition: {
+    onHover: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
   },
 };
