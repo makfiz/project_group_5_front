f248584102203caefcd36c52b6cd6c6e788a43af
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-02-26 13:59:12 +0200

    fix falidation

--- src/utils/loginValidationSchema.js
+++ src/utils/loginValidationSchema.js
@@ -1,21 +1,22 @@
 import * as yup from 'yup';
 
-yup.addMethod(yup.string, 'numeric', function () {
-  return this.matches(/^\d+$/, 'The number should have digits only');
-});
-
-yup.addMethod(yup.string, 'leters', function () {
-  return this.matches(/^[aA-zZ\s]+$/, 'The email should have only alphabets');
+yup.addMethod(yup.string, 'password', function () {
+  return this.matches(/^\S+$/, 'password must be without spaces');
 });
 
 yup.addMethod(yup.string, 'mail', function () {
   return this.matches(
-    /^[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    /^((?!-)([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     'The email address is incorect'
   );
 });
 
 export const schema = yup.object().shape({
   email: yup.string().mail().required('Please enter your email!'),
-  password: yup.string().min('7').required('Please enter your password!'),
+  password: yup
+    .string()
+    .password()
+    .min('7')
+    .max('32')
+    .required('Please enter your password!'),
 });
