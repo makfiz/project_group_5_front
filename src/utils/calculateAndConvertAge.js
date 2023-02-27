8ea15aa5739060b910c52615c0f357f696569708
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-02-27 12:02:08 +0200

    fix Notices Age

--- src/utils/calculateAndConvertAge.js
+++ src/utils/calculateAndConvertAge.js
@@ -1,8 +1,10 @@
-import { formatDistanceToNowStrict } from 'date-fns';
+import { formatDistanceToNowStrict, format } from 'date-fns';
 import converter from 'number-to-words';
+import moment from 'moment';
 
 export const calculateAndConvertAge = dateOfBirth => {
-  const ageWithDigit = formatDistanceToNowStrict(dateOfBirth);
+  const date = moment(dateOfBirth, 'DD/MM/YYYY').toDate();
+  const ageWithDigit = formatDistanceToNowStrict(date);
   const ageInRow = ageWithDigit.split(' ');
   const ageInWord = converter.toWords(parseInt(ageInRow[0]));
   ageInRow.splice(0, 1, ageInWord);
