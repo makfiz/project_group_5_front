14322018dc0042589b790626960a6e430f447efb
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-02-27 14:57:41 +0200

    fix

--- src/components/SharedLayout/SharedLayout.js
+++ src/components/SharedLayout/SharedLayout.js
@@ -2,14 +2,11 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Header } from '../Header/Header';
 
-
 export const SharedLayout = ({ toggleTheme, theme }) => {
-
   return (
     <>
       <Header toggleTheme={toggleTheme} theme={theme} />
-      <Suspense
-      
+      <Suspense>
         <Outlet />
       </Suspense>
     </>
