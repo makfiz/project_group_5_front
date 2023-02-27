6676b5453e251d59a3539fb9c543f664fb092aa3
Yanik Rikhter <rihter.yan@gmail.com>
2023-02-27 19:11:37 +0200

    fixes of header menu and addpetMobile button to
    removed local state to redux, added conditional render for addPetMobileButton so that it won't show up when menu is open

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,38 +25,45 @@ import closeMenuSvg from '../../assets/menu-icons/close-menu.svg';
 // import { NavLink } from 'react-router-dom';
 import { Navigation } from './Navigation/Nav/Nav';
 import { AuthNav } from './Navigation/AuthNav/AuthNav';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { UserNav } from './Navigation/UserNav/UserNav';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
 import ThemeToggle from 'components/ThemeToggle/ThemeToggle';
+import { toggleMenu } from 'redux/menu/menuSlice';
 
 export const Header = ({ toggleTheme, theme }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const menuState = useSelector(state => state.menuState.isMenuOpen);
+
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
   //   const handleMenuClick = {};
   // const isLoggedIn = false;
   useEffect(() => {
-    if (isMenuOpen) {
+    if (menuState) {
       document.body.classList.add('no-scroll');
     } else {
       document.body.classList.remove('no-scroll');
     }
-  }, [isMenuOpen]);
+  }, [menuState]);
 
   const handleCloseMenu = () => {
-    setIsMenuOpen(false);
+    dispatch(toggleMenu());
+    // setIsMenuOpen(false);
   };
   const handleOpenMenu = () => {
-    setIsMenuOpen(true);
+    // setIsMenuOpen(true);
+    dispatch(toggleMenu());
   };
 
   const handleMenu = () => {
-    setIsMenuOpen(isMenuOpen === false ? true : false);
+    // setIsMenuOpen(isMenuOpen === false ? true : false);
+    dispatch(toggleMenu());
   };
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <Container>
-      {isMenuOpen ? (
+      {menuState ? (
         <>
           <MenuActive>
             {isLoggedIn ? (
