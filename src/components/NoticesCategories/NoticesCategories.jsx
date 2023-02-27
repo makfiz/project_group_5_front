41ff46371f9daa8a3e79c40654688ad103ccb6db
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-02-27 09:32:41 +0200

    add learn more spinner, fix modal scroll

--- src/components/NoticesCategories/NoticesCategories.jsx
+++ src/components/NoticesCategories/NoticesCategories.jsx
@@ -63,6 +63,7 @@ export const NoticesCategories = () => {
             token ? handleModalToggle() : navigate('/login');
           }}
         >
+          Add pet
           <AddPetBtnIconWrap>
             <PlusIcon />
           </AddPetBtnIconWrap>
