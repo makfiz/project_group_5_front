6cdffc50aeb423411027154dd748fb6e8ac1b4d8
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-02-27 18:02:34 +0200

    fix

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,11 +1,12 @@
 import { Suspense, useEffect, useState } from 'react';
 import * as Scroll from 'react-scroll';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import { NoticesSearch } from 'components/NoticesSearch/NoticesSearch';
 import { NoticesCategories } from 'components/NoticesCategories/NoticesCategories';
 import { PaginationNotices } from 'components/PaginationNotices/PaginationNotices';
 import { LernMoreModal } from 'components/LernMoreModal/LernMoreModal';
-import { useSelector } from 'react-redux';
 import { Container } from 'components/Container/Container';
 
 import { ToTopButton, Icon } from './NoticesPage.styled';
@@ -13,16 +14,16 @@ import { ToTopButton, Icon } from './NoticesPage.styled';
 const scroll = Scroll.animateScroll;
 
 const NoticesPage = () => {
-  const [visibe, setVisible] = useState(true);
+  const [visibe, setVisible] = useState(false);
   const itemNotice = useSelector(state => state.notices.notice);
 
   const handleScroll = () => {
     const position = window.pageYOffset;
 
-    if (position >= 500) {
+    if (position >= 1000) {
       setVisible(true);
     }
-    if (position <= 500) {
+    if (position <= 1000) {
       setVisible(false);
     }
   };
@@ -34,7 +35,7 @@ const NoticesPage = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [scroll]);
+  }, []);
 
   const toTopButton = () => {
     scroll.scrollToTop();
